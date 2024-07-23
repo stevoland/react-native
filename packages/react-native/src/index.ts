360d793438ae9099667372534f10e7d21dfb4d3a
Stephen Collings <stephen@mettle.co.uk>
2024-07-23 22:59:12 +0100

    fix: Workaround Cannot read property __STORYBOOK_ADDON_INTERACTIONS_INSTRUMENTER_STATE__

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -2,6 +2,10 @@ import type { StorybookConfig as StorybookConfigBase } from '@storybook/core/typ
 import type { ReactNativeOptions } from './Start';
 export { darkTheme, theme, type Theme } from '@storybook/react-native-theming';
 
+// @ts-ignore Workaround [TypeError: Cannot read property '__STORYBOOK_ADDON_INTERACTIONS_INSTRUMENTER_STATE__' of undefined]
+// from @storybook/test
+global.window.parent = global.window.parent || {};
+
 export { start, prepareStories, getProjectAnnotations, updateView } from './Start';
 
 export interface StorybookConfig {
